f27b7ddee545712cbd2f9d2fe8e990903e2bc5c2
Xron Trix <xrontrix@gmail.com>
2024-02-14 00:08:35 +0530

    Kept only Get Method

--- src/main/java/com/attendy/attendy/controller/MainController.java
+++ src/main/java/com/attendy/attendy/controller/MainController.java
@@ -1,7 +1,6 @@
 package com.attendy.attendy.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,10 +9,5 @@ public class MainController {
     @GetMapping("/")
     public String root() {
         return "Server is up and running !";
-    }
-
-    @PostMapping("/post")
-    public String post() {
-        return "Post is also working !!";
     }
 }
